afaf5b5a51648f5f9ba20adfa39dc871f4d1adba
Bill Thaman <william.thaman@gmail.com>
2022-02-18 16:06:51 -0600

    modified error message for better troubleshooting

--- traverse_v4.go
+++ traverse_v4.go
@@ -110,7 +110,7 @@ func trace(starting_edge string, base_edges []string, m map[string][]string, m_e
 	depth++
 	if depth > 5000 {
 		fmt.Println("It appears the network has a loop. The last edge processed is: ", starting_edge)
-		fmt.Println("base edges: ", base_edges)
+		fmt.Println("base edges (portion of network that is looped): ", base_edges)
 		os.Exit(99)
 	}
 	// parent edges are those where their downstream node (tnode) is equal to the starting_edge upstream node (fnode)
